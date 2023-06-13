0db5c1e1ba3dbfd9dcb6c7787741493cfa964763
juliaepihina <86781452+juliaepihina@users.noreply.github.com>
2023-06-13 19:14:41 +0300

    Add files via upload

---
+++ meter/levenshtein.h
@@ -0,0 +1,10 @@
+#ifndef ASSEMBLYTESTING_LEVENSHTEIN_H
+#define ASSEMBLYTESTING_LEVENSHTEIN_H
+
+#include <string>
+
+using std::string;
+
+double editing_distance(const string &source_dna, const string &result_dna);
+
+#endif //ASSEMBLYTESTING_LEVENSHTEIN_H
